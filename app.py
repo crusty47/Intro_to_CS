3511ae5f57fa1a047f948cd4777e19d3dc869b59
andres <andres.montemayor02@utrgv.edu>
2022-11-29 08:33:42 -0600

    intital commit

---
+++ app.py
@@ -0,0 +1,21 @@
+# Import necessary packages and functions
+from flask import Flask, render_template
+
+# Create our web application
+app = Flask(__name__)
+
+# Create our routes
+@app.route('/')
+def home():
+	return render_template('index.html')
+
+@app.route('/about')
+def about():
+	return render_template('aboutpage.html')
+
+# Start our application
+if __name__ == "__main__":
+	app.run(
+		debug=True,
+		port = 8888
+	)
